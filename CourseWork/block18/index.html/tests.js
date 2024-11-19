e2f195ddefaa7535efbf791bb050d4b91167eda7
Janaya Cooper <janayacooper@janayas-mbp.mynetworksettings.com>
2024-11-18 21:09:55 -0500

    multiplication and unit test

---
+++ CourseWork/block18/index.html/tests.js
@@ -0,0 +1,32 @@
+//Expect [action] to be [some result]
+//Example: For the prompt "A function called "addition" that returns the sum of two input integers", your tests might include:
+//Expect addition(2, 3) to be a number
+//Expect addition(2, 3) to be equal to 5
+//Expect addition("a", 3) to be an error
+
+
+// Unit Test input numbers
+const multiply =(x,y) =>{
+    const product = x * y;
+    return product;
+}
+    multiply(3,9);
+//Test cases
+//Expect multiply to return the  product of (x,y)
+//Expect multiply to return  a number 
+//Expect multiply (3,9) to be equal to 27
+//Expect multiply("a", 3) to be an error
+
+// Unit Test two arrays 
+
+const concatOdds (array1, array2);
+ const catOdds ([3,2,1], [9,1,1,1,4,15,-1])
+ return catOdds [-1,1,3,9,15]
+// Expect the function concatOdds take two array integers 
+//Expect the  return to be a  a single array of odd numbers in ascending order. 
+//Expect if unexpected input number is not in the array then to result in an error
+// Expect all the numbers in the result of the array to have no repetition of the numbers.
+//Expect with using an array numbers cannot include a zero number between -1 and 1. 
+//Expect results of the array to only be odd numbers. 
+
+// Functional Test
